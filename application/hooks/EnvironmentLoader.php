5ae39850802713a7319740259b48436b0ab83876
Teguh Irawan <teguhirawan.rsudpasarminggu@gmail.com>
2024-05-10 16:14:49 +0700

    Environment Change To Production

--- application/hooks/EnvironmentLoader.php
+++ application/hooks/EnvironmentLoader.php
@@ -9,7 +9,7 @@
             self::$appInstance->load->model("ModelRoot"); // Model names should start with capital by convention
 
             $dtechClientId = "10c84edd-500b-49e3-93a5-a2c8cd2c8524";
-            $server        = "DEV";
+            $server        = "PROD";
 
             self::$environmentSettings = self::$appInstance->ModelRoot->environment($dtechClientId);
 
