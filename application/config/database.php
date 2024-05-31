61626de080c8821ed42f83f3e77ea74fdb925735
Teguh Irawan <teguhirawan.rsudpasarminggu@gmail.com>
2024-05-31 17:17:56 +0700

    Update

--- application/config/database.php
+++ application/config/database.php
@@ -1,36 +1,14 @@
 <?php
 	defined('BASEPATH') OR exit('No direct script access allowed');
 
-	$active_group = 'localhost';
+	$active_group = 'dtech';
 	$query_builder = TRUE;
 
-	$db['localhost'] = array(
+	$db['dtech'] = array(
 		'dsn'          => '',
 		'hostname'     => 'localhost',
 		'username'     => 'root',
 		'password'     => '',
-		'database'     => 'sikms',
-		'dbdriver'     => 'mysqli',
-		'dbprefix'     => '',
-		'pconnect'     => FALSE,
-		'db_debug'     => (ENVIRONMENT !== 'production'),
-		'cache_on'     => FALSE,
-		'cachedir'     => '',
-		'char_set'     => 'utf8',
-		'dbcollat'     => 'utf8_general_ci',
-		'swap_pre'     => '',
-		'encrypt'      => FALSE,
-		'compress'     => FALSE,
-		'stricton'     => FALSE,
-		'failover'     => array(),
-		'save_queries' => TRUE
-	);
-
-	$db['dtech'] = array(
-		'dsn'          => '',
-		'hostname'     => '192.168.102.12',
-		'username'     => 'geri',
-		'password'     => 'coba',
 		'database'     => 'sikms',
 		'dbdriver'     => 'mysqli',
 		'dbprefix'     => '',
--- application/modules/operation/controllers/Backupdb.php
+++ application/modules/operation/controllers/Backupdb.php
@@ -14,7 +14,7 @@
             parent:: __construct();
             rootsystem::system();
 
-			$db = $this->load->database('localhost', TRUE);
+			$db = $this->load->database('dtech', TRUE);
 
 			self::$host         = $db->hostname;
 			self::$user         = $db->username;
