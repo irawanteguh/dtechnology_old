6d2cf21c5a45684de2850a39ac9f73a862ad3b00
Teguh Irawan <teguhirawan.rsudpasarminggu@gmail.com>
2024-07-02 23:22:23 +0700

    Update Query Monev

--- application/modules/monev/models/Modelkunjunganyears.php
+++ application/modules/monev/models/Modelkunjunganyears.php
@@ -18,15 +18,13 @@
             $query =
                     "
                         WITH monthly_data AS (
-                            SELECT 
-                                DATE_FORMAT(tgl_registrasi, '%m.%Y') AS periode,
-                                COUNT(IF(status_lanjut = 'Ralan', 1, NULL)) AS jmlrj,
-                                COUNT(IF(status_lanjut = 'Ranap', 1, NULL)) AS jmlri
-                            FROM 
-                                reg_periksa a
-                            WHERE DATE_FORMAT(tgl_registrasi, '%Y') = '".$periode."'
-                            GROUP BY 
-                                DATE_FORMAT(tgl_registrasi, '%m.%Y')
+                            select  date_format(tgl_registrasi, '%m.%Y') periode,
+                                    count(IF(status_lanjut = 'Ralan', 1, NULL)) jmlrj,
+                                    count(IF(status_lanjut = 'Ranap', 1, NULL)) jmlri
+                            from reg_periksa a
+                            where date_format(a.tgl_registrasi, '%Y') = '".$periode."'
+                            and   a.stts<>'Batal'
+                            group by date_format(tgl_registrasi, '%m.%Y')
                         ),
                         total_counts AS (
                             SELECT 
@@ -56,19 +54,17 @@
         function kunjunganpoli($periode){
             $query =
                     "
-                        select date_format(tgl_registrasi, '%Y') periode, a.kd_poli,
-                            count(no_rawat) AS jmltahunini,
-                            (select count(no_rawat) from reg_periksa where status_lanjut=a.status_lanjut and kd_poli=a.kd_poli and year(tgl_registrasi)='".$periode."' - 1)jmlthnlalu,
+                        select date_format(tgl_registrasi, '%Y') periode, kd_poli, count(no_rawat) jmltahunini,
+                            (select count(no_rawat) from reg_periksa  where status_lanjut=a.status_lanjut and stts=a.stts and kd_poli=a.kd_poli and year(tgl_registrasi) = year(a.tgl_registrasi) - 1) jmlthnlalu,
                             (select upper(nm_poli) from poliklinik where kd_poli = a.kd_poli) AS namapoli
                                 
                         from reg_periksa a
                         where a.status_lanjut = 'Ralan'
                         and a.kd_poli <>'-'
+                        and a.stts<>'Batal'
                         and date_format(tgl_registrasi, '%Y') = '".$periode."'
                         group by periode, a.kd_poli
-                        order by jmltahunini desc;
-
-
+                        order by jmltahunini desc
                     ";
 
             $recordset = $this->db->query($query);
