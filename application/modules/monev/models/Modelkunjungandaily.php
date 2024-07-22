06966bd35583e7ba99ebd6c4f1d753c767929a37
Teguh Irawan <teguhirawan.rsudpasarminggu@gmail.com>
2024-07-22 22:59:23 +0700

    Update

--- application/modules/monev/models/Modelkunjungandaily.php
+++ application/modules/monev/models/Modelkunjungandaily.php
@@ -5,13 +5,16 @@
         function totalkunjungan(){
             $query =
                     "
-                        select tgl_registrasi, date_format(tgl_registrasi,'%d.%m.%Y')periode, date_format(tgl_registrasi,'%d.%m')label,
-                            count(IF(status_lanjut = 'Ralan', 1, NULL)) jmlrj,
-                            count(IF(status_lanjut = 'Ranap', 1, NULL)) jmlri
-                            
-                        from reg_periksa a
-                        group by date_format(tgl_registrasi,'%d.%m.%Y')
-                        order by tgl_registrasi asc
+                        select x.*
+                        from(
+                            select tgl_registrasi, date_format(tgl_registrasi,'%d.%m.%Y')periode, date_format(tgl_registrasi,'%d.%m')label,
+                                count(IF(status_lanjut = 'Ralan', 1, NULL)) jmlrj,
+                                count(IF(status_lanjut = 'Ranap', 1, NULL)) jmlri
+                                
+                            from reg_periksa a
+                            group by date_format(tgl_registrasi,'%d.%m.%Y')
+                            order by tgl_registrasi asc
+                        )x
                         limit 30;
                     ";
 
