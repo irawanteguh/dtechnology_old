bb777530832dca6aa478713cd08a267e6135e56f
Teguh Irawan <teguhirawan.rsudpasarminggu@gmail.com>
2024-07-23 16:07:38 +0700

    Update

--- application/config/config.php
+++ application/config/config.php
@@ -34,7 +34,7 @@
     $config['sess_save_path']          = APPPATH.'sessions';                                                                                                                                                                          // Path ini harus valid
     $config['sess_match_ip']           = FALSE;
     $config['sess_time_to_update']     = 300;
-    $config['sess_regenerate_destroy'] = FALSE;
+    $config['sess_regenerate_destroy'] = TRUE;
     $config['cookie_prefix']           = '';
     $config['cookie_domain']           = '';
     $config['cookie_path']             = '/';
