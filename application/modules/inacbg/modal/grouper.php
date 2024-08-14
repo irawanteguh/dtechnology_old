a8ca07452e012d07fbaf75b8f8ab3286d8f38bff
Teguh Irawan <teguhirawan.rsudpasarminggu@gmail.com>
2024-08-14 22:17:01 +0700

    Update Modules Inacbgs

--- application/modules/inacbg/modal/grouper.php
+++ application/modules/inacbg/modal/grouper.php
@@ -14,7 +14,199 @@
                     <div class="text-muted fw-bold fs-5">Grouper Inacbg's</div>
                 </div>
                 <div class="d-flex justify-content-center">
-                    
+                    <div class="row d-flex justify-content-center">
+
+                        <div class="row">
+                            <h1 class="mb-5">Patient Information</h1>
+                            <div class="col-md-2 mb-5">
+                                <label class="d-flex align-items-center fs-5 fw-bold mb-2 required">Medical Record</label>
+                                <input type="text" class="form-control form-control-solid" id="data_position_name_add" name="data_position_name_add" required>
+                            </div>
+                            <div class="col-md-4 mb-5">
+                                <label class="d-flex align-items-center fs-5 fw-bold mb-2 required">Patient Name</label>
+                                <input type="text" class="form-control form-control-solid" id="data_position_name_add" name="data_position_name_add" required>
+                            </div>
+                            <div class="col-md-2 mb-5">
+                                <label class="d-flex align-items-center fs-5 fw-bold mb-2 required">Sex</label>
+                                <input type="text" class="form-control form-control-solid" id="data_position_name_add" name="data_position_name_add" required>
+                            </div>
+                            <div class="col-md-4 mb-5">
+                                <label class="d-flex align-items-center fs-5 fw-bold mb-2 required">Address</label>
+                                <input type="text" class="form-control form-control-solid" id="data_position_name_add" name="data_position_name_add" required>
+                            </div>
+                        </div>
+                        <div class="mt-5 row">
+                            <h1 class="mb-5">Transaction Information</h1>
+                            <div class="col-md-2 mb-5">
+                                <label class="d-flex align-items-center fs-5 fw-bold mb-2 required">No Transaction</label>
+                                <input type="text" class="form-control form-control-solid" id="data_position_name_add" name="data_position_name_add" required>
+                            </div>
+                            <div class="col-md-2 mb-5">
+                                <label class="d-flex align-items-center fs-5 fw-bold mb-2 required">Transaction Date</label>
+                                <input type="text" class="form-control form-control-solid" id="data_position_name_add" name="data_position_name_add" required>
+                            </div>
+                            <div class="col-md-2 mb-5">
+                                <label class="d-flex align-items-center fs-5 fw-bold mb-2 required">Type</label>
+                                <input type="text" class="form-control form-control-solid" id="data_position_name_add" name="data_position_name_add" required>
+                            </div>
+                            <div class="col-md-3 mb-5">
+                                <label class="d-flex align-items-center fs-5 fw-bold mb-2 required">Polyclinic / Room</label>
+                                <input type="text" class="form-control form-control-solid" id="data_position_name_add" name="data_position_name_add" required>
+                            </div>
+                            <div class="col-md-3 mb-5">
+                                <label class="d-flex align-items-center fs-5 fw-bold mb-2 required">Doctor Name</label>
+                                <input type="text" class="form-control form-control-solid" id="data_position_name_add" name="data_position_name_add" required>
+                            </div>
+                            <div class="col-md-2 mb-5">
+                                <label class="d-flex align-items-center fs-5 fw-bold mb-2 required">Provider</label>
+                                <input type="text" class="form-control form-control-solid" id="data_position_name_add" name="data_position_name_add" required>
+                            </div>
+                            <div class="col-md-2 mb-5">
+                                <label class="d-flex align-items-center fs-5 fw-bold mb-2 required">Insurance No</label>
+                                <input type="text" class="form-control form-control-solid" id="data_position_name_add" name="data_position_name_add" required>
+                            </div>
+                            <div class="col-md-4 mb-5">
+                                <label class="d-flex align-items-center fs-5 fw-bold mb-2 required">Insurance Policy Approval Letter</label>
+                                <input type="text" class="form-control form-control-solid" id="data_position_name_add" name="data_position_name_add" required>
+                            </div>
+                            <div class="col-md-2 mb-5">
+                                <label class="d-flex align-items-center fs-5 fw-bold mb-2 required">Class</label>
+                                <input type="text" class="form-control form-control-solid" id="data_position_name_add" name="data_position_name_add" required>
+                            </div>
+                            <div class="col-md-2 mb-5">
+                                <label class="d-flex align-items-center fs-5 fw-bold mb-2 required">Discharge Date</label>
+                                <input type="text" class="form-control form-control-solid" id="data_position_name_add" name="data_position_name_add" required>
+                            </div>
+                            <div class="col-md-2 mb-5">
+                                <label class="d-flex align-items-center fs-5 fw-bold mb-2 required">ADL Sub Acute</label>
+                                <input type="text" class="form-control form-control-solid" id="data_position_name_add" name="data_position_name_add" required>
+                            </div>
+                            <div class="col-md-2 mb-5">
+                                <label class="d-flex align-items-center fs-5 fw-bold mb-2 required">ADL Chronic</label>
+                                <input type="text" class="form-control form-control-solid" id="data_position_name_add" name="data_position_name_add" required>
+                            </div>
+                            <div class="col-md-2 mb-5">
+                                <label class="d-flex align-items-center fs-5 fw-bold mb-2 required">ICU Indikator</label>
+                                <input type="text" class="form-control form-control-solid" id="data_position_name_add" name="data_position_name_add" required>
+                            </div>
+                            <div class="col-md-2 mb-5">
+                                <label class="d-flex align-items-center fs-5 fw-bold mb-2 required">ICU LOS</label>
+                                <input type="text" class="form-control form-control-solid" id="data_position_name_add" name="data_position_name_add" required>
+                            </div>
+                            <div class="col-md-2 mb-5">
+                                <label class="d-flex align-items-center fs-5 fw-bold mb-2 required">Ventilator</label>
+                                <input type="text" class="form-control form-control-solid" id="data_position_name_add" name="data_position_name_add" required>
+                            </div>
+                            <div class="col-md-2 mb-5">
+                                <label class="d-flex align-items-center fs-5 fw-bold mb-2 required">Indikator Upgrade Kelas</label>
+                                <input type="text" class="form-control form-control-solid" id="data_position_name_add" name="data_position_name_add" required>
+                            </div>
+                            <div class="col-md-2 mb-5">
+                                <label class="d-flex align-items-center fs-5 fw-bold mb-2 required">Naik Kelas</label>
+                                <input type="text" class="form-control form-control-solid" id="data_position_name_add" name="data_position_name_add" required>
+                            </div>
+                            <div class="col-md-2 mb-5">
+                                <label class="d-flex align-items-center fs-5 fw-bold mb-2 required">Lama Naik Kelas</label>
+                                <input type="text" class="form-control form-control-solid" id="data_position_name_add" name="data_position_name_add" required>
+                            </div>
+                            <div class="col-md-2 mb-5">
+                                <label class="d-flex align-items-center fs-5 fw-bold mb-2 required">Biaya Tambahan</label>
+                                <input type="text" class="form-control form-control-solid" id="data_position_name_add" name="data_position_name_add" required>
+                            </div>
+                            <div class="col-md-2 mb-5">
+                                <label class="d-flex align-items-center fs-5 fw-bold mb-2 required">Berat Saat Lahir</label>
+                                <input type="text" class="form-control form-control-solid" id="data_position_name_add" name="data_position_name_add" required>
+                            </div>
+                            <div class="col-md-2 mb-5">
+                                <label class="d-flex align-items-center fs-5 fw-bold mb-2 required">Sistol</label>
+                                <input type="text" class="form-control form-control-solid" id="data_position_name_add" name="data_position_name_add" required>
+                            </div>
+                            <div class="col-md-2 mb-5">
+                                <label class="d-flex align-items-center fs-5 fw-bold mb-2 required">Diastol</label>
+                                <input type="text" class="form-control form-control-solid" id="data_position_name_add" name="data_position_name_add" required>
+                            </div>
+                            <div class="col-md-2 mb-5">
+                                <label class="d-flex align-items-center fs-5 fw-bold mb-2 required">Status Pulang</label>
+                                <input type="text" class="form-control form-control-solid" id="data_position_name_add" name="data_position_name_add" required>
+                            </div>
+                            <div class="col-md-2 mb-5">
+                                <label class="d-flex align-items-center fs-5 fw-bold mb-2 required">Diagnosa</label>
+                                <input type="text" class="form-control form-control-solid" id="data_position_name_add" name="data_position_name_add" required>
+                            </div>
+                            <div class="col-md-8 mb-5">
+                                <label class="d-flex align-items-center fs-5 fw-bold mb-2 required">procedure</label>
+                                <input type="text" class="form-control form-control-solid" id="data_position_name_add" name="data_position_name_add" required>
+                            </div>
+                        </div>
+                        <div class="mt-5 row">
+                            <h1 class="mb-5">Hospital Fee Bills</h1>
+                            <div class="col-md-2 mb-5">
+                                <label class="d-flex align-items-center fs-5 fw-bold mb-2 required">Non Surgical Procedure</label>
+                                <input type="text" class="form-control form-control-solid" id="data_position_name_add" name="data_position_name_add" required>
+                            </div>
+                            <div class="col-md-2 mb-5">
+                                <label class="d-flex align-items-center fs-5 fw-bold mb-2 required">Surgical Procedures</label>
+                                <input type="text" class="form-control form-control-solid" id="data_position_name_add" name="data_position_name_add" required>
+                            </div>
+                            <div class="col-md-2 mb-5">
+                                <label class="d-flex align-items-center fs-5 fw-bold mb-2 required">Consultation</label>
+                                <input type="text" class="form-control form-control-solid" id="data_position_name_add" name="data_position_name_add" required>
+                            </div>
+                            <div class="col-md-2 mb-5">
+                                <label class="d-flex align-items-center fs-5 fw-bold mb-2 required">Experts</label>
+                                <input type="text" class="form-control form-control-solid" id="data_position_name_add" name="data_position_name_add" required>
+                            </div>
+                            <div class="col-md-2 mb-5">
+                                <label class="d-flex align-items-center fs-5 fw-bold mb-2 required">Nursing</label>
+                                <input type="text" class="form-control form-control-solid" id="data_position_name_add" name="data_position_name_add" required>
+                            </div>
+                            <div class="col-md-2 mb-5">
+                                <label class="d-flex align-items-center fs-5 fw-bold mb-2 required">Support</label>
+                                <input type="text" class="form-control form-control-solid" id="data_position_name_add" name="data_position_name_add" required>
+                            </div>
+                            <div class="col-md-2 mb-5">
+                                <label class="d-flex align-items-center fs-5 fw-bold mb-2 required">Radiology</label>
+                                <input type="text" class="form-control form-control-solid" id="data_position_name_add" name="data_position_name_add" required>
+                            </div>
+                            <div class="col-md-2 mb-5">
+                                <label class="d-flex align-items-center fs-5 fw-bold mb-2 required">Laboratory</label>
+                                <input type="text" class="form-control form-control-solid" id="data_position_name_add" name="data_position_name_add" required>
+                            </div>
+                            <div class="col-md-2 mb-5">
+                                <label class="d-flex align-items-center fs-5 fw-bold mb-2 required">Pharmacy / Medicine</label>
+                                <input type="text" class="form-control form-control-solid" id="data_position_name_add" name="data_position_name_add" required>
+                            </div>
+                            <div class="col-md-2 mb-5">
+                                <label class="d-flex align-items-center fs-5 fw-bold mb-2 required">Chronic Medicine</label>
+                                <input type="text" class="form-control form-control-solid" id="data_position_name_add" name="data_position_name_add" required>
+                            </div>
+                            <div class="col-md-2 mb-5">
+                                <label class="d-flex align-items-center fs-5 fw-bold mb-2 required">Chemotherapy Drugs</label>
+                                <input type="text" class="form-control form-control-solid" id="data_position_name_add" name="data_position_name_add" required>
+                            </div>
+                            <div class="col-md-2 mb-5">
+                                <label class="d-flex align-items-center fs-5 fw-bold mb-2 required">Blood Transfusion</label>
+                                <input type="text" class="form-control form-control-solid" id="data_position_name_add" name="data_position_name_add" required>
+                            </div>
+                            <div class="col-md-2 mb-5">
+                                <label class="d-flex align-items-center fs-5 fw-bold mb-2 required">Rehabilitation</label>
+                                <input type="text" class="form-control form-control-solid" id="data_position_name_add" name="data_position_name_add" required>
+                            </div>
+                            <div class="col-md-2 mb-5">
+                                <label class="d-flex align-items-center fs-5 fw-bold mb-2 required">Room</label>
+                                <input type="text" class="form-control form-control-solid" id="data_position_name_add" name="data_position_name_add" required>
+                            </div>
+                            <div class="col-md-2 mb-5">
+                                <label class="d-flex align-items-center fs-5 fw-bold mb-2 required">Intensive Care</label>
+                                <input type="text" class="form-control form-control-solid" id="data_position_name_add" name="data_position_name_add" required>
+                            </div>
+                            <div class="col-md-2 mb-5">
+                                <label class="d-flex align-items-center fs-5 fw-bold mb-2 required">Medical devices</label>
+                                <input type="text" class="form-control form-control-solid" id="data_position_name_add" name="data_position_name_add" required>
+                            </div>
+                        </div>
+                        
+                    </div>
                 </div>
             </div>
             <div class="modal-footer p-1">	
