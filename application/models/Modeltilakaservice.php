c69c0d6eee09eee935a9e08549079fcfbb1c7bc3
Irawanteguh <teguhirawan.rsudpasarminggu@gmail.com>
2024-08-26 07:51:07 +0700

    Update

--- application/models/Modeltilakaservice.php
+++ application/models/Modeltilakaservice.php
@@ -79,7 +79,6 @@
                                 (select name from dt01_gen_user_data   where org_id=a.org_id and active='1' and nik=a.assign)assignname
                         from dt01_gen_document_file_dt a
                         where a.active='1'
-                        and   a.user_identifier='cakraandhika02'
                         and   a.org_id='".$orgid."'
                         and   a.status_sign ='4'
                     ";
